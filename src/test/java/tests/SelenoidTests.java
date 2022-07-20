6e7963ceca60f540d7ba7f943e2da27c64e614ce
Koten <Kotenkiton@gmail.com>
2022-07-20 10:18:12 +0300

    get fix

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -1,16 +1,20 @@
 package tests;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelenoidTests {
-  /*
-        1. Cделай запрос такой то make request to https://selenoid.autotests.cloud/status
-        2.  Получи такой то ответ. get response {"total":20,"used":0,"queued":0,"pending":0,"browsers":
+    /*
+        1. make request to https://selenoid.autotests.cloud/status
+        2. get response {"total":20,"used":0,"queued":0,"pending":0,"browsers":
             {"chrome":{"100.0":{},"99.0":{}},"firefox":{"97.0":{},"98.0":{}},"opera":{"84.0":{},"85.0":{}}}}
-        3. Ассерт.check total is 20
+        3. check total is 20
      */
 
     @Test
@@ -24,9 +28,102 @@ public class SelenoidTests {
 
     @Test
     void checkWithoutGivenTotal() {
-                get("https://selenoid.autotests.cloud/status")
+        get("https://selenoid.autotests.cloud/status")
                 .then()
                 .body("total", is(20));
     }
+
+    @Test
+    void checkWithLogsTotal() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkWithSomeLogsTotal() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .body("total", is(20));
+    }
+
+
+    @Test
+    void checkChrome() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalBadPractice() {
+        Response response = get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract().response();
+
+        System.out.println(response.asString());
+
+        String expectedResponse = "{\"total\":20,\"used\":0,\"queued\":0,\"pending\":0,\"browsers\":{" +
+                "\"chrome\":{\"100.0\":{},\"99.0\":{}}," +
+                "\"firefox\":{\"97.0\":{},\"98.0\":{}}," +
+                "\"opera\":{\"84.0\":{},\"85.0\":{}}}}\n";
+
+        assertEquals(expectedResponse, response.asString());
+    }
+
+    @Test
+    void checkTotalGoodPractice() {
+        Integer actualTotal = get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract().path("total");
+
+        System.out.println(actualTotal);
+
+        Integer expectedTotal = 20;
+        assertEquals(expectedTotal, actualTotal);
+    }
+
+    @Test
+    void checkWithStatusTotal() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void check401StatusTotal() {
+        get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    void check200StatusWithAuthInUrlTotal() {
+        get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    void check200StatusWithAuthTotal() {
+        given()
+                .auth().basic("user1", "1234")
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+
 
 }
